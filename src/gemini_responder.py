54a3cb5401fff24fec5d39149fc22bc371504a67
seolheun36 <seolheun36@gmail.com>
2025-04-24 14:10:46 +0900

    gemini model change

--- src/gemini_responder.py
+++ src/gemini_responder.py
@@ -8,7 +8,7 @@ class GeminiResponder:
     def __init__(
         self,
         api_key=None,
-        model_name="gemini-2.0-flash-thinking-exp-01-21",
+        model_name="gemini-2.5-flash-preview-04-17",
         temperature=1,
         top_p=0.95,
         top_k=64,
